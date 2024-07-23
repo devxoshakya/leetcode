3c2c7f09fab2379cc93395b34cfa434aef40d2f4
Dev Shakya <devxoshakya@gmail.com>
2024-07-23 10:17:03 +0530

    Time: 0 ms (100%), Space: 44.6 MB (19.33%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,53 +11,17 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if(head == null) return null;
-        ListNode fast = head;
-        ListNode slow = head;
-        int lengthCycle = 0;
-        while(fast.next != null && fast !=null && fast.next.next!=null){
-            fast = fast.next.next;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
-            if(fast == slow){
-                lengthCycle = length(slow);
-                break;
-            }
-        }
-
-
-        if(lengthCycle == 0){
-            return null;
-        }
-
-        ListNode first = head;
-        ListNode second = slow;
-
-        while(first!=second){
-            first = first.next;
-            second = second.next;
-        }
-
-        return first;
-            
-
-    }
-
-    private int length(ListNode head){
-        ListNode fast = head;
-        ListNode slow = head;
-        while(fast.next != null && fast !=null){
             fast = fast.next.next;
-            slow = slow.next;
-            if(fast == slow){
-                ListNode temp = slow;
-                int length = 0;
-                do {
-                    temp = temp.next;
-                    length++;
-                } while(temp!=slow);
-                return length;
-            }
+            if (slow == fast) break;
         }
-        return 0;
+        if (fast == null || fast.next == null) return null;
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
     }
 }
