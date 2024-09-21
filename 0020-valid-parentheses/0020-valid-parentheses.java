cbdc7575f8799ecf08a559a6165534865e03277c
Dev Shakya <devxoshakya@gmail.com>
2024-09-21 09:59:09 +0530

    Time: 2 ms (52.33%), Space: 41.2 MB (84.32%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+     public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            if(c == ')' || c == '}' || c == ']'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char popped = stack.pop();
+                if(c == ')' && popped != '('){
+                    return false;
+                }
+                if(c == '}' && popped != '{'){
+                    return false;
+                }
+                if(c == ']' && popped != '['){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
