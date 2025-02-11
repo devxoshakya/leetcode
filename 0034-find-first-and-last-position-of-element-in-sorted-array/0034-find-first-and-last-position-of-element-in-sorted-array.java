078daf61ca14a9ca8c3ed7f8109ba1f91350f1f6
Dev Shakya <devxoshakya@gmail.com>
2025-02-11 21:07:55 +0530

    Time: 0 ms (100%), Space: 46.1 MB (29.61%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,32 @@
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+        
+//     }
+// }
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1,-1};
-        ans[0] = search(nums,target,true);
-        if(ans[0]!=-1){
-            ans[1] = search(nums,target,false);
+        if (nums.length == 0) return new int[]{-1, -1};
+        
+        // Find first and last position using 2 binary searches
+        int first = binarySearch(nums, target - 0.5);
+        int last = binarySearch(nums, target + 0.5) - 1;
+        
+        if (first <= last) {
+            return new int[]{first, last};
         }
-        return ans;
+        return new int[]{-1, -1};
     }
-    static int search(int[] nums, int target, boolean check){
-        int start = 0;
-        int end = nums.length-1;
-        int ans = -1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(target<nums[mid]){
-                end = mid-1;
-            }
-            else if (target>nums[mid]){
-                start = mid +1;
-            }else{
-                ans = mid;
-                if(check){
-                    end=mid-1;
-                }else{
-                    start=mid+1;
-                }
+    
+    private int binarySearch(int[] nums, double target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        return ans;
+        return left;
     }
 }
