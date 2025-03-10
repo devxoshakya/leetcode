65eced224882af060a820c5fe4de943fd15f5250
Dev Shakya <devxoshakya@gmail.com>
2025-03-10 10:36:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x>=0){
-            return (x==rev(x));
-        }
         
-        return false;
-    }
-    private int rev(int n){
-        int sum = 0;
-        while(n>0){
-            int rem = n%10;
-            sum = sum*10 + rem;
-            n = n/10;
-        }
-        return sum;
     }
 }
