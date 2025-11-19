8deb640122a5cabebbe3db2081cd83738f0b058b
Dev Shakya <devxoshakya@gmail.com>
2025-11-19 10:07:45 +0530

    Time: 24 ms (74.3%), Space: 80.5 MB (21.3%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,8 +1,33 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-          Arrays.sort(nums);
-         return nums;
+        if(nums == null || nums.length <= 1){
+            return nums;
+        }
+        mergeSort(nums, 0, nums.length - 1, new int[nums.length]);
+        return nums;
     }
 
-    
+    private void mergeSort(int[] a, int l, int r, int[] temp){
+        if(l >= r) return;
+        int mid = l + (r - l) / 2;
+        mergeSort(a, l, mid, temp);
+        mergeSort(a, mid + 1, r, temp);
+        merge(a, l, mid, r, temp);
+    }
+
+    private void merge(int[] a, int l, int mid, int r, int[] temp){
+        int i = l, j = mid + 1, k = l;
+        while(i <= mid && j <= r){
+            if(a[i] <= a[j]){
+                temp[k++] = a[i++];
+            }else{
+                temp[k++] = a[j++];
+            }
+        }
+        while(i <= mid) temp[k++] = a[i++];
+        while(j <= r) temp[k++] = a[j++];
+        for(k = l; k <= r; k++){
+            a[k] = temp[k];
+        }
+    }
 }
