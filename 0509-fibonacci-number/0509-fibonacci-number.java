940fea426dbcae7e516a68c560d6bbd96c0fdcb7
Dev Shakya <devxoshakya@gmail.com>
2025-11-24 09:41:26 +0530

    Time: 0 ms (100%), Space: 41.9 MB (22.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,16 @@
 class Solution {
+    static HashMap<Integer,Integer> cache = new HashMap<>();
     public int fib(int n) {
-        if(n<2) return n;
-        return fib(n-1) + fib(n-2);
+        if(n < 2){
+            return n;
+        }
+
+        if(cache.containsKey(n)){
+            return cache.get(n);
+        }
+
+        int result = fib(n-1) + fib(n-2);
+        cache.put(n,result);
+        return result;
     }
 }
