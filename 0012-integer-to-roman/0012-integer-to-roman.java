9248ab49499013027274eec0f91e7856f384a156
Dev Shakya <devxoshakya@gmail.com>
2026-01-30 16:04:28 +0530

    Time: 8 ms (7.66%), Space: 46.6 MB (37.98%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,32 +1,32 @@
 class Solution {
     public String intToRoman(int num) {
-        Map<Integer, String> romanMap = new LinkedHashMap<>() {
-            {
-                put(1000, "M");
-                put(900, "CM");
-                put(500, "D");
-                put(400, "CD");
-                put(100, "C");
-                put(90, "XC");
-                put(50, "L");
-                put(40, "XL");
-                put(10, "X");
-                put(9, "IX");
-                put(5, "V");
-                put(4, "IV");
-                put(1, "I");
-            }
-        };
+        Map<Integer, String> romanMap = new LinkedHashMap<>() {{
+            put(1000, "M");
+            put(900, "CM");
+            put(500, "D");
+            put(400, "CD");
+            put(100, "C");
+            put(90, "XC");
+            put(50, "L");
+            put(40, "XL");
+            put(10, "X");
+            put(9, "IX");
+            put(5, "V");
+            put(4, "IV");
+            put(1, "I");
+        }};
 
-        String res = "";
-        for (int i : romanMap.keySet()) {
-            if (num / i != 0) {
-                int count = num / i;
-                res += romanMap.get(i).repeat(count);
-                num %= i;
+        StringBuilder res = new StringBuilder();
+
+        for (Map.Entry<Integer, String> entry : romanMap.entrySet()) {
+            int value = entry.getKey();
+            String symbol = entry.getValue();
+
+            while (num >= value) {
+                res.append(symbol);
+                num -= value;
             }
         }
-        return res;
-
+        return res.toString();
     }
 }
