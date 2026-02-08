a122ae7469c450eb76e6a2f93fbc77700115f8ec
Dev Shakya <devxoshakya@gmail.com>
2026-02-08 20:28:23 +0530

    Time: 1 ms (99.63%), Space: 47 MB (43.27%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> l = new ArrayList<>();
+        int i = 1;
+        while ( i <= n){
+            if(i % 3 == 0 && i % 5 == 0){
+                l.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                l.add("Fizz");
+            } else if ( i % 5 == 0){
+                l.add("Buzz");
+            } else {
+                l.add(Integer.toString(i));
+            }
+
+           i++;
+        }
+        return l;
+    }
+}
