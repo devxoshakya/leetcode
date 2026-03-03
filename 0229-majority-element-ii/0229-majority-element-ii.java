2573209794c512413eb816f1c304cbf12d10db6c
Dev Shakya <devxoshakya@gmail.com>
2026-03-03 17:42:11 +0530

    Time: 16 ms (21.89%), Space: 50.7 MB (99.2%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int limit = nums.length/3 + 1;
+        int limit = nums.length / 3;
         List<Integer> res = new ArrayList<>();
-        HashMap<Integer,Integer> freq = new HashMap<>();
-        for(int i : nums){
-            freq.put(i, freq.getOrDefault(i,0)+1);
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i : nums) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+
+            if(freq.size() <= 2) continue;
+
+            HashMap<Integer, Integer> dummy = new HashMap<>();
+            for(int key : freq.keySet()){
+                if(freq.get(key) > 1){
+                    dummy.put(key,freq.get(key)-1);
+                }
+            }
+            freq = dummy;
         }
 
-        for(int i : freq.keySet()){
-            if(freq.get(i) >= limit ){
-                res.add(i);
-            }   
+        for(int key : freq.keySet()){
+            if(count(nums,key) > limit){
+                res.add(key);
+            }
         }
+
         return res;
+    }
+
+    private int count(int[] nums, int k) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == k) {
+                count++;
+            }
+        }
+        return count;
     }
 }
