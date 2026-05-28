69be190c31ae209bd08a66cb1fee1c2c101730db
Dev Shakya <devxoshakya@gmail.com>
2026-05-28 11:22:53 +0530

    Time: 2319 ms (5%), Space: 61.2 MB (5.57%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> set = new HashSet<>();
+
         Arrays.sort(nums);
 
-        for(int i=0; i<nums.length-2; i++){
-            HashSet<Integer> seen = new HashSet<>();
-            for(int j = i + 1; j < nums.length; j++){
-                int complement = -nums[i] - nums[j];
-                if(seen.contains(complement)){
-                    set.add(Arrays.asList(nums[i],nums[j],complement));
+        for(int i=0; i < nums.length; i++){
+            Set<Integer> seen = new HashSet<>();
+            for(int j=i+1; j < nums.length; j++){
+                int third = - (nums[i] + nums[j]);
+                if(seen.contains(third)){
+                    set.add(Arrays.asList(nums[i],third,nums[j]));
                 }
-            seen.add(nums[j]);
+                seen.add(nums[j]);
             }
         }
 
         return new ArrayList<>(set);
-    }
-
-     static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter f = new FileWriter("display_runtime.txt")) {
-                f.write("1520");
-            } catch (Exception e) {
-
-            }
-        }));
     }
 }
