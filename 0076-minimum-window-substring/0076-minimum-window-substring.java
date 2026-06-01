21e4602e23c51b2f7fa95218ae064155fd7a6e60
Dev Shakya <devxoshakya@gmail.com>
2026-06-01 13:11:56 +0530

    Time: 20 ms (40.17%), Space: 47.8 MB (19.63%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,48 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length())
-            return "";
-        if (s.equals(t))
-            return s;
+        if (t.isEmpty()) return "";
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        Map<Character,Integer> countT = new HashMap<>();
+        Map<Character,Integer> window = new HashMap<>();
+        int res[] = {-1,-1};
+        int len = Integer.MAX_VALUE; 
+
+        for(char ch : t.toCharArray()){
+            countT.put(ch,countT.getOrDefault(ch,0)+1);
+        }
+
+        int have = 0; int need = countT.size();
         int l = 0;
-        int r = 0;
-        int minLength = Integer.MAX_VALUE;
-        int minStart = 0;
-        int count = 0;
+        for(int i=0; i < s.length(); i++){
+            char c = s.charAt(i);
+            window.put(c,window.getOrDefault(c,0)+1);
 
-        for (char ch : t.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-        while (r < s.length()) {
-            char c = s.charAt(r);
-            map.put(c, map.getOrDefault(c, 0) - 1);
-            if (map.get(c) >= 0) {
-                count++;
+            if(countT.containsKey(c) && window.get(c).equals(countT.get(c))){
+                have++;
             }
 
-            while (count == t.length()) {
-                if (r - l + 1 < minLength) {
-                    minLength = r - l + 1;
-                    minStart = l;
+            while(have == need){
+                if(i-l+1 < len){
+                    res[0] = l;
+                    res[1] = i;
+                    len = i - l + 1;
                 }
-
-                char left = s.charAt(l);
-                if (map.containsKey(left)) {
-                    map.put(left, map.get(left) + 1);
-                    if (map.get(left) > 0) {
-                        count--;
-                    }
+                char key = s.charAt(l);
+                window.put(key,window.get(key) - 1);
+                if(countT.containsKey(key) && window.get(key) < countT.get(key)){
+                    have--;
                 }
-                l++;
+                l++; 
             }
-            r++;
 
         }
-
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(minStart,minStart + minLength);
+            return len != Integer.MAX_VALUE ? s.substring(res[0],res[1]+1) : "";
 
     }
 }
