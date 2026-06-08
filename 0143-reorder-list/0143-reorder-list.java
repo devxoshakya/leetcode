cd62e5a2e298f70d2f24e646e635aad15231eb92
Dev Shakya <devxoshakya@gmail.com>
2026-06-08 12:16:02 +0530

    Time: 1 ms (99.91%), Space: 50.1 MB (10.53%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,50 +9,26 @@
  * }
  */
 class Solution {
+    ListNode curr;
+    boolean abort;
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
-        }
-        ListNode mid = midNode(head);
-        ListNode headSecond = reverseList(mid);
-        ListNode headFirst = head;
-        while(headSecond != null && headFirst != null){
-            ListNode temp1 = headFirst.next;
-            ListNode temp2 = headSecond.next;
-            headFirst.next = headSecond;
-            headSecond.next = temp1;
-            headFirst = temp1;
-            headSecond = temp2;
-        }
-
-        if(headFirst != null){
-            headFirst.next = null;
-        }
-
+        curr = head;
+        abort = false;
+        reorder(curr);
     }
 
-    private ListNode midNode(ListNode head) {
-        if (head.next == null) {
-            return head;
+    private void reorder(ListNode node){
+        if(node == null) return;
+        reorder(node.next);
+        if(!abort && (curr == node || curr.next == node)){
+            node.next = null;
+            abort = true;
         }
-        ListNode fast = head;
-        ListNode slow = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
+        if(abort) return;
 
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        ListNode next = curr.next;
+        curr.next = node;
+        node.next = next;
+        curr = next;
     }
 }
