e9a5666534decccd8f734f6b0dd867a9e96ab8f8
Dev Shakya <devxoshakya@gmail.com>
2026-06-27 12:04:50 +0530

    Time: 120 ms (93.37%), Space: 43 MB (59.56%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,32 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
+        int rows = board.length;
+        int cols = board[0].length;
 
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (exist(board, word, row, col, 0)) {
+                    return true;
                 }
             }
         }
-        
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
-            return true;
-        }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+
+    private boolean exist(char[][] board, String word, int i, int j, int idx){
+        if( i < 0 || j < 0 || i >= board.length ||  j >= board[0].length || word.charAt(idx) != board[i][j] 
+        || board[i][j] == '#' ) {
             return false;
         }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+
+        if(idx == word.length()-1) return true;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean result = exist(board,word,i+1,j,idx+1) ||  exist(board,word,i,j+1,idx+1) || exist(board,word,i-1,j,idx+1) || exist(board,word,i,j-1,idx+1);
+
+        board[i][j] = temp;
+        return result;
     }
 }
