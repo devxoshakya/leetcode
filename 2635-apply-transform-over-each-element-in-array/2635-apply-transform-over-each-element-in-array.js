4cf9ed59aed96afbf3303d7dee031a3c1d4e557c
Dev Shakya <devxoshakya@gmail.com>
2026-07-02 10:07:30 +0530

    Time: 46 ms (40.59%), Space: 53.3 MB (70.53%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,8 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    for(let i = 0; i < arr.length; i++){
-        arr[i] = fn(arr[i],i);
+    const result = [];
+    for(let i=0; i < arr.length; i++){
+        result.push(fn(arr[i],i));
     }
-    return arr;
+    return result;
 };
