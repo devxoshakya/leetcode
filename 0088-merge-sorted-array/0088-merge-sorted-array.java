5657c7fd2dc4c014aedeaddd875d73f84e45f6f5
Dev Shakya <devxoshakya@gmail.com>
2026-07-26 12:30:54 +0530

    Time: 0 ms (100%), Space: 43.6 MB (77.95%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -11,5 +11,13 @@ class Solution {
                 nums1[k--] = nums2[j--];
             }
         }
+
+        // while(j >= 0){
+        //     if(i >= 0 && nums1[i] > nums2[j]){
+        //         nums1[k--] = nums1[j--];
+        //     } else {
+        //         nums1[k--] = nums2[j--];
+        //     }
+        // }
     }
 }
