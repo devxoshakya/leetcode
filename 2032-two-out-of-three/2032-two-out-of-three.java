5e9adddf69d3dd3acac90636ddcdc9752512ea0b
Dev Shakya <devxoshakya@gmail.com>
2026-07-27 09:49:07 +0530

    Time: 1 ms (100%), Space: 46.5 MB (89.36%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+         boolean[] b1 = new boolean[101];
+        boolean[] b2 = new boolean[101];
+        boolean[] b3 = new boolean[101];
+
+        /** where values of the boolean array 
+         * at the corresponding index are changed to true */
+        for (int num : nums1) b1[num] = true;
+
+        for (int num : nums2) b2[num] = true;
+
+        for (int num : nums3) b3[num] = true;
+
+        List<Integer> res = new ArrayList<>();
+
+
+        for (int i = 1; i <= 100; i++){
+            if((b1[i] || b2[i]) && (b2[i] || b3[i]) && (b3[i] || b1[i]))
+                res.add(i);
+        }
+        return res;
+    }
+}
