12271b33398cee26cf6f299605850234bddf986e
Dev Shakya <devxoshakya@gmail.com>
2026-08-10 10:31:02 +0530

    Time: 19 ms (67.43%), Space: 52.6 MB (63.12%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,21 +1,47 @@
 class MyHashMap {
-    int[] data;
+    List<int[]>[] chain;
+    int size = 13000;
+
+    public int getHashcode(int key){
+        return key % size;
+    }
 
     public MyHashMap() {
-        data = new int[1000001];
-        Arrays.fill(data,-1);
+        chain = new ArrayList[size];
+        for(int i=0; i < size; i++){
+            chain[i] = new ArrayList<>();
+        }
     }
     
     public void put(int key, int value) {
-        data[key] = value;
+        int hash = getHashcode(key);
+        for(int i=0; i < chain[hash].size(); i++){
+            if(chain[hash].get(i)[0] == key){
+                chain[hash].get(i)[1] = value;
+                return;
+            }
+        }
+        chain[hash].add(new int[]{key,value});
     }
     
     public int get(int key) {
-        return data[key];
+        int hash = getHashcode(key);
+        for(int i=0; i < chain[hash].size(); i++){
+            if(chain[hash].get(i)[0] == key){
+                return chain[hash].get(i)[1];
+            }
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        data[key] = -1;
+         int hash = getHashcode(key);
+        for(int i=0; i < chain[hash].size(); i++){
+            if(chain[hash].get(i)[0] == key){
+               chain[hash].remove(i);
+            }
+        }
+        return;
     }
 }
 
